3d908bec3c548a45a6363b2ae02caf0c1cc5eb62
wxn0brP <wxn0brP@users.noreply.github.com>
2026-01-06 16:27:17 +0000

    deploy: 741ed5b5b441cc0e8ee21c35558f9bc1e3df2c99

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,25 @@
-async function d(){try{let e=+await(await fetch("https://wxn0brp.github.io/paper/posts.txt")).text();return localStorage.setItem("newsCount",e.toString()),e}catch{if(localStorage.getItem("newsCount"))return+localStorage.getItem("newsCount")||0;return 0}}async function u(t){let s;if(localStorage.getItem(`post-${t}`))s=localStorage.getItem(`post-${t}`)||"";else s=await(await fetch(`https://wxn0brp.github.io/paper/post/${t}.txt`)).text(),localStorage.setItem(`post-${t}`,s);let e=s.split(`
-`),n=e.shift(),o=e.shift(),r=e.shift(),w=e.shift(),l=e.join(`
-`);return{id:t,date:o,title:n,url:r,icon:w,content:l}}function i(){let t=localStorage.getItem("viewedNews");if(!t)return[];return t.split(",")}function c(...t){let e=[...i(),...t],n=new Set(e);a.viewedNews=[...n],localStorage.setItem("viewedNews",[...n].join(","))}function p(t,s){let e=document.querySelector("#list");if(!e)return;e.innerHTML="",t.forEach((n)=>{let o=document.createElement("article");o.className="news-card",o.classList.toggle("viewed",s.includes(n.id)),o.innerHTML=`
-            <div class="news-top-section">
-                <div class="news-info">
-                    <h2 class="news-title">${n.title}</h2>
-                    <div class="news-meta">
-                        <span class="news-date">${new Date(n.date).toLocaleString()||""}</span>
-                        ${n.url?`<a href="${n.url}" class="news-source-link" target="_blank">Source</a>`:""}
-                    </div>
-                </div>
-                ${n.icon?`<div class="news-icon-wrapper"><img class="news-icon" src="${n.icon}" alt=""></div>`:""}
-            </div>
-            <div class="news-content-section">
-                <details class="news-details">
-                    <summary class="news-summary">Read more</summary>
-                    <div class="news-text">
-                        <p>${n.content}</p>
-                    </div>
-                </details>
-            </div>
-        `,o.addEventListener("click",()=>{c(n.id),o.classList.toggle("viewed",!0)}),e.appendChild(o)})}async function g(){let t=[];for(let s=a.newsCount;s>=1;s--)t.push(await u(s.toString()));return t}var a={newsCount:0,news:[],viewedNews:[]};async function m(){a.newsCount=await d(),a.viewedNews=i(),a.news=await g(),p(a.news,a.viewedNews),console.log(a)}var f=m();window.store=a;window.addEventListener("message",async(t)=>{if(t.data.type==="get-count")t.source.postMessage({type:"count",count:a.newsCount-a.viewedNews.length});if(t.data.type==="mark-as-viewed")c(...t.data.ids),t.source.postMessage({type:"viewed-news",news:a.viewedNews});if(t.data.type==="ready")await f,t.source.postMessage({type:"ready"});if(t.data.type==="set-css-var"){let s=t.data.name,e=t.data.value;document.documentElement.style.setProperty("--"+s,e)}if(t.data.type==="get-store")t.source.postMessage({type:"store",store:a})});
+async function w(){try{let s=+await(await fetch("https://wxn0brp.github.io/paper/posts.txt")).text();return localStorage.setItem("newsCount",s.toString()),s}catch{if(localStorage.getItem("newsCount"))return+localStorage.getItem("newsCount")||0;return 0}}async function u(t){let e;if(localStorage.getItem(`post-${t}`))e=localStorage.getItem(`post-${t}`)||"";else e=await(await fetch(`https://wxn0brp.github.io/paper/post/${t}.txt`)).text(),localStorage.setItem(`post-${t}`,e);let s=e.split(`
+`),n=s.shift(),o=s.shift(),i=s.shift(),r=s.shift(),l=s.join(`
+`);return{id:t,date:o,title:n,url:i,icon:r,content:l}}function c(){let t=localStorage.getItem("viewedNews");if(!t)return[];return t.split(",")}function d(...t){let s=[...c(),...t],n=new Set(s);a.viewedNews=[...n],localStorage.setItem("viewedNews",[...n].join(","))}function m(t,e){let s=document.querySelector("#list");if(!s)return;s.innerHTML="",t.forEach((n)=>{let o=document.createElement("article");o.className="news-card",o.classList.toggle("viewed",e.includes(n.id)),o.innerHTML=`
+<div class="news-top-section">
+    <div class="news-info">
+        <h2 class="news-title">${n.title}</h2>
+        <div class="news-meta">
+            <span class="news-date">${new Date(n.date).toLocaleString()||""}</span>
+            ${n.url?`<a href="${n.url}" class="news-source-link" target="_blank">Source</a>`:""}
+        </div>
+    </div>
+    ${n.icon?`<div class="news-icon-wrapper"><img class="news-icon" src="${n.icon}" alt=""></div>`:""}
+</div>
+<div class="news-content-section">
+    <details class="news-details">
+        <summary class="news-summary">Read more</summary>
+        <div class="news-text">
+            <p>${n.content}</p>
+        </div>
+    </details>
+</div>
+        `,o.addEventListener("click",()=>{d(n.id),o.classList.toggle("viewed",!0)}),s.appendChild(o)})}async function g(){let t=[];for(let e=a.newsCount;e>=1;e--)t.push(await u(e.toString()));return t}var a={newsCount:0,news:[],viewedNews:[]};async function f(){a.newsCount=await w(),a.viewedNews=c(),a.news=await g(),m(a.news,a.viewedNews)}f();var p=new URLSearchParams(window.location.search);for(let[t,e]of p.entries())document.documentElement.style.setProperty("--"+t,e);
 
-//# debugId=DE7B028306BDC91964756E2164756E21
+//# debugId=BD1DFC28B2C2F83A64756E2164756E21
 //# sourceMappingURL=index.js.map
--- dist/load.js
+++ dist/load.js
@@ -1,4 +1,4 @@
-(()=>{let e=document.createElement("iframe");e.src="https://wxn0brp.github.io/paper/",e.style.display="none",e.id="small-paper-iframe",document.body.appendChild(e);let t=document.createElement("button");t.id="small-paper-button",t.innerHTML="V",t.title="View News",document.body.appendChild(t),t.addEventListener("click",()=>{e.style.display=e.style.display==="none"?"":"none",e.contentWindow.postMessage({type:"get-count"},"*")}),window.addEventListener("message",(o)=>{let n=o.data;if(n.type==="ready")e.contentWindow.postMessage({type:"get-count"},"*");if(n.type==="count")t.innerHTML="V"+n.count.toString()}),e.addEventListener("load",()=>{setTimeout(()=>{e.contentWindow.postMessage({type:"ready"},"*")},100)})})();
+(()=>{let n=document.currentScript;if(!n)return;let r=new URL(n.src).searchParams,e=document.createElement("iframe");e.src="https://wxn0brp.github.io/paper/?"+r.toString(),e.style.display="none",e.id="small-paper-iframe",document.body.appendChild(e);let t=document.createElement("button");t.id="small-paper-button",t.innerHTML="V",t.title="View News",document.body.appendChild(t),t.addEventListener("click",()=>{e.style.display=e.style.display==="none"?"":"none"})})();
 
-//# debugId=755C8C2C53F9551164756E2164756E21
+//# debugId=5C47FF1971C8F62C64756E2164756E21
 //# sourceMappingURL=load.js.map
